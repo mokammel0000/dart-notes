69346ca5e831f93734aba6a14f235beb95b17b37
Mohamed Mostafa <mokammel0000@gmail.com>
2024-04-30 02:16:26 +0300

    Adding platform methods

---
+++ 01_platform/lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  // print(Platform.environment);
+  // contain all data about your working environment
+
+  // Map<String, String> env = Platform.environment;
+  // env.forEach((key, value) => print(key + ' is: ' + value));
+
+  // print('-------------------------------------------');
+  // print(Platform.numberOfProcessors);
+  // print(Platform.operatingSystem);
+  // print(Platform.operatingSystemVersion);
+  // print(Platform.localeName); // device language
+  // print(Platform.localHostname); // current user name
+  // print('-------------------------------------------');
+
+  // print(Platform.executable); // where is dart sdk installed
+  // print(Platform.script); // location of this dart file
+  // print(Platform.version); // version of dart sdk
+  // print('-------------------------------------------');
+
+  // print(Platform.isAndroid);
+  // print(Platform.isIOS);
+  // print(Platform.isWindows);
+  // print(Platform.isLinux);
+  // print(Platform.isMacOS);
+  // print(Platform.isFuchsia); // google operating system
+  // print(Platform.isAndroid);
+}
