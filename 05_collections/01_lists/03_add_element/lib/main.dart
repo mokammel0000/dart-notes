b3f835a55877c6c356f2f74319aad2fd19a84a38
Mohamed Mostafa <mokammel0000@gmail.com>
2024-05-04 01:11:00 +0300

    collections
    list - set - map

---
+++ 05_collections/01_lists/03_add_element/lib/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<int> list = [0, 0, 10, 20, 30, 40, 50];
+  // list.add(50); // add to the end of the list
+  // print(list);
+
+  // list.addAll([50, 60, 70]); // add list or set to another list
+  // print(list);
+
+  // list.insert(3, 100); // add at some index, and shift other elements
+  // print(list);
+
+  // list.insertAll(0, {15, 25, 35}); //add at some index, and shift other elements
+  // print(list);
+}
