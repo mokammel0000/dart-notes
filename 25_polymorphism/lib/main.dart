0b5760bc669ee333f315f9c8d9b5690d49d37e4a
Mohamed Mostafa <mokammel0000@gmail.com>
2024-06-01 18:21:10 +0300

    polymorphism

---
+++ 25_polymorphism/lib/main.dart
@@ -0,0 +1,15 @@
+/** 
+ * Polymorphism: تعددية الأوجه/ إن الحاجه يكون لها كذا شكل
+ * 
+ * ممكن يتطبق على: 
+ * - Classes: 
+ *    -one object can act as a type of it's parent
+ *    -List<Shape> shapes = [shape1, circle1, rectangel1, squar1];
+ * 
+ * - Override: 
+ *    -one method can behave different behaviors(according to it's object)
+ * 
+ * - Overloading: 
+ *    -one method can have multiple signatures
+ *    -one operator can behabe different behaviors.
+ */
